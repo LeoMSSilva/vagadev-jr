90f07165d9549e6098cd2e04bfac88d419f3ab5f
LeoMSSilva <l.eom1994@hotmail.com>
2021-05-28 08:29:12 -0300

    Fix: position of footer

--- n1rush/src/assets/components/footer.jsx
+++ n1rush/src/assets/components/footer.jsx
@@ -2,16 +2,16 @@ import '../scss/index.scss';
 import logo from '../imgs/n1.png';
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="footer-left">
-                <img src={logo} alt="Agência n1" />
-            </div>
-            <div className="footer-right">
-                <p>Agência N1 - Todos os direitos reservados</p>
-            </div>
-        </footer>
-    );
+	return (
+		<footer>
+			<div className="footer-left">
+				<img src={logo} alt="Agência n1" />
+			</div>
+			<div className="footer-right">
+				<p>Agência N1 - Todos os direitos reservados</p>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
